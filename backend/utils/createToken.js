0812db51479284548ecfd9a9f13c977aa246cb03
Nzoputachi <samuelnzoputachi@gmail.com>
2024-07-03 03:47:09 +0100

    Add error handling with try...catch block

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -1,17 +1,26 @@
+// 26:12 mins 
+
 import jwt from 'jsonwebtoken'
 
 const generateToken = (res, userId) => {
-    const token = jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn: "30d"})
+    try {
+        const token = jwt.sign({userId: userId._id}, process.env.JWT_SECRET, {expiresIn: "30d"});
 
-    //set JWT to an HTTP-only cookie
-    res.cookie('jwt', token, {
-        httpOnly: true, 
-        secure: process.env.NODE_ENV != 'development',
-        sameSite: 'strict',
-        maxAge: 30 * 24 * 60 * 60 * 1000
-    })
-
-    return token;
-}
+        //set jet as HTTP-only Cookie
+        res.cookie('jwt', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== 'development' ,
+            sameSite: 'strict',
+            maxAge: 30 * 24 * 60 * 60 * 1000
+        })
+    
+        return token;
+    } catch (error) {
+        console.error('Error generating token: ', error);
+        throw new Error ('Failed to generate jrk token')
+    }
+};
 
 export default generateToken;
+
+// {userId: userId.username}
